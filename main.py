e9fce53b8afe7092304d826ba9b107825741b260
Eric Timmer <eric.h.timmer@gmail.com>
2024-01-19 21:41:09 +0100

    19-3

--- main.py
+++ main.py
@@ -68,15 +68,62 @@ def wifi_setup():  #Exit of Routine: wifi_status() = 1 means connected, 0 mean n
             break
     return(wifi_status)
 
+def wifi_check():
+    station = network.WLAN(network.STA_IF)
+    print("Check if connected to Wifi")
+    if station.isconnected() == True:
+        print('Still connected to:',ssid)
+        wifi_status.on()  #Wifi LED is put ON
+    else:
+        print('Reconnect to Wifi')
+        wifi_setup()
+        #wifi_status updated from wifi_setup() routine
+        # Needs WORK HERE
+        #IF wifi_status = 1 continue program with Blynk Connected
+        #IF Wifi_status = 0 continue without Wifi and Blynk
+    print('Test wifi_check status:',wifi_status())
+    return(wifi_status)
+
+def read_dht():   #From DHT-11 temp/humidity module
+    global temp, hum
+#    dht_pin = dht.DHT22(Pin(4))  # Comment it if you are using DHT22 and uncomment the next line
+    dht_pin = dht.DHT11(Pin(4)) # Uncomment it if you are using DHT11 and comment the above line
+    temp = hum = 0
+    try:
+        sleep(1.25) # Sample period is 1 sec
+        dht_pin.measure()
+        temp = dht_pin.temperature()
+        hum = dht_pin.humidity()
+        if (isinstance(temp, float) and isinstance(hum, float)) or (isinstance(temp, int) and isinstance(hum, int)):
+            msg = (b'{0:3.1f},{1:3.1f}'.format(temp, hum))
+            hum = round(hum, 2)
+            return(msg, temp, hum)
+        else:
+            return('Invalid DHT sensor readings.')
+    except OSError as e:
+        return('Failed to read DHT sensor.')
+
+def read_moist():  #From FC-28 moisture sensor module
+    global moi
+    moi = 0
+    moisture = ADC(0)
+    sleep(1.25)
+    moi = moisture.read()
+    moi = round((100*((1024 - moi)/556)))  #Moisture in %, Must be calibrated !!!
+    return(moi) 
+
+start = 1   # Start and keep running
+
 ########################################################################################
 ###                          MAIN PROGRAM STARTS HERE                                ###
 ########################################################################################
+input('Wait here before starting program')
 
 # Setup Wifi 1st time after fresh start
 wifi_setup()
+
 # Wifi ON: Status=1 or OFF: Status=0
 # Blue LED is ON when connected
-
 if wifi_status() == 1:
     print('Wifi Status = ON')
 else:
@@ -99,9 +146,10 @@ if OTA.update():
     input('Update done')
     machine.reset()
     
-
+print('No update done !!!')
 if OTA.fetch():
     print("A newer version is available!")
 else:
     print("Up to date!")
     
+
--- test2.py
+++ test2.py
@@ -1,1 +1,2 @@
 print('Test2')
+#UPDATE 2
