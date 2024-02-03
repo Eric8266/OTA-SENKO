911efb2949a5f43b4d4aedc595881311dbaca9f8
Eric Timmer <eric.h.timmer@gmail.com>
2024-02-03 14:50:56 +0100

    3feb-14.50

--- test2.py
+++ test2.py
@@ -1,4 +1,6 @@
+#################################################################
 print('Only Test2 updated in main dir on Github, 3 Feb 2024.')
-#UPDATE 4!!!!: 3 Feb 2024
+#UPDATE 5: 3 Feb 2024
+print('Testing delay from Github')
 #Yepp, het wekt nog.
 #################################################################
